d67affb752a1948ff0c52ffc605492848998b7f1
Marti Raudsepp <marti@juffo.org>
2018-04-12 01:04:57 +0300

    Crap

--- src/entity/inventory.js
+++ src/entity/inventory.js
@@ -6,6 +6,7 @@ import { vec4 } from 'gl-matrix';
 // TODO: Deprecate in favor of a proper scene graph
 var InventoryItems = {};
 
+/*
 var simple = {
   Xmp: 'L8',
   Ultrastrike: 'L8',
@@ -22,6 +23,7 @@ var simple = {
   Resonator: 'L8',
   Capsule: 'RARE'
 };
+*/
 
 export function createItemEntity(name, color) {
 
@@ -30,15 +32,11 @@ export function createItemEntity(name, color) {
       super(engine);
       this.addDrawable(name, new Inventory[name]());
       this.addDrawable(name + 'Xm', new Inventory[name + 'Xm']());
-      this.drawables[name].uniforms.u_color0 = vec4.clone(color);
+      this.drawables[name].uniforms.u_color0 = vec4.clone(Constants.qualityColors[color]);
     }
   }
 
   return entitybase;
-}
-
-for(var i in simple) {
-  InventoryItems[i] = createItemEntity(i, Constants.qualityColors[simple[i]]);
 }
 
 class FlipCardAda extends Entity {
@@ -52,8 +50,6 @@ class FlipCardAda extends Entity {
   }
 }
 
-InventoryItems.FlipCardAda = FlipCardAda;
-
 class FlipCardJarvis extends Entity {
   constructor(engine) {
     super(engine);
@@ -64,8 +60,6 @@ class FlipCardJarvis extends Entity {
     this.drawables.FlipCardJarvis.uniforms.u_color0 = vec4.clone(Constants.qualityColors.VERY_RARE);
   }
 }
-
-InventoryItems.FlipCardJarvis = FlipCardJarvis;
 
 class ExtraShield extends Entity {
   constructor(engine) {
@@ -108,18 +102,44 @@ class PortalKeyResourceUnit extends Entity {
 
 InventoryItems.PortalKeyResourceUnit = PortalKeyResourceUnit;
 
-class KeyCapsule extends Entity {
-  constructor(engine) {
-    super(engine);
-    this.addDrawable('KeyCapsule', new Inventory.KeyCapsule());
-    this.addDrawable('KeyCapsuleXm', new Inventory.KeyCapsuleXm());
-    this.drawables.KeyCapsule.uniforms.u_color0 = vec4.clone(Constants.keyCapsuleColors.blue[0]);
-    this.drawables.KeyCapsule.uniforms.u_color1 = vec4.clone(Constants.keyCapsuleColors.blue[1]);
-    this.drawables.KeyCapsuleXm.uniforms.u_teamColor = vec4.clone(Constants.xmColors.coreGlowChaotic);
-    this.drawables.KeyCapsuleXm.uniforms.u_altColor = vec4.clone(Constants.xmColors.coreGlowChaoticAlt);
+function createKeyCapsule(color) {
+  class Locker extends Entity {
+    constructor(engine) {
+      super(engine);
+      this.addDrawable('KeyCapsule', new Inventory.KeyCapsule());
+      this.addDrawable('KeyCapsuleXm', new Inventory.KeyCapsuleXm());
+      this.drawables.KeyCapsule.uniforms.u_color0 = vec4.clone(Constants.keyCapsuleColors[color][0]);
+      this.drawables.KeyCapsule.uniforms.u_color1 = vec4.clone(Constants.keyCapsuleColors[color][1]);
+      this.drawables.KeyCapsuleXm.uniforms.u_teamColor = vec4.clone(Constants.xmColors.coreGlowChaotic);
+      this.drawables.KeyCapsuleXm.uniforms.u_altColor = vec4.clone(Constants.xmColors.coreGlowChaoticAlt);
+    }
   }
+  return Locker;
 }
 
-InventoryItems.KeyCapsule = KeyCapsule;
+InventoryItems = {
+  "XMP Burster L8": createItemEntity('Xmp', 'L8'),
+  "XMP Burster L4": createItemEntity('Xmp', 'L4'),
+  "XMP Burster L1": createItemEntity('Xmp', 'L1'),
+  "Ultra Strike L8": createItemEntity('Ultrastrike', 'L8'),
+  "Ultra Strike L4": createItemEntity('Ultrastrike', 'L4'),
+  "Ultra Strike L1": createItemEntity('Ultrastrike', 'L1'),
+  "Power Cube L8": createItemEntity('PowerCube', 'L8'),
+  "Power Cube L4": createItemEntity('PowerCube', 'L4'),
+  "Power Cube L1": createItemEntity('PowerCube', 'L1'),
+  "Lawson Power Cube": createItemEntity('BoostedPowerCube', 'VERY_RARE'),
+  "Circle-K Power Cube": createItemEntity('BoostedPowerCubeK', 'VERY_RARE'),
+  "Portal Shield Common": createItemEntity('ResShield', 'COMMON'),
+  "Portal Shield Rare": createItemEntity('ResShield', 'RARE'),
+  "Portal Shield Very Rare": createItemEntity('ResShield', 'VERY_RARE'),
+  "AXA/Aegis Shield": ExtraShield,
+  "ADA Refactor": FlipCardAda,
+  "JARVIS Virus": FlipCardJarvis,
+  "Key Locker Green": createKeyCapsule('green'),
+  "Key Locker Blue": createKeyCapsule('blue'),
+  "Key Locker White": createKeyCapsule('white'),
+  "Key Locker Red": createKeyCapsule('red'),
+  "Key Locker Yellow": createKeyCapsule('yellow'),
+};
 
 export default InventoryItems;
